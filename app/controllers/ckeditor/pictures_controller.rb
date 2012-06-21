b6424ecbcc603e549624854e3fc6e5be06326f44
Javier <javier.poo@gmail.com>
2012-06-21 12:35:12 -0400

    picture assets management

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,23 +1,34 @@
 class Ckeditor::PicturesController < Ckeditor::ApplicationController
 
+  before_filter :init_site
+
   def index
-    @pictures = Ckeditor.picture_model.find_all(ckeditor_pictures_scope)
-    respond_with(@pictures) 
+    @pictures = Ckeditor.picture_model.find_all(ckeditor_pictures_scope) if @site
+    respond_with(@pictures)
   end
-  
+
   def create
-    @picture = Ckeditor::Picture.new
-	  respond_with_asset(@picture)
+    @picture = Assets::Picture.new( :site_id => @site.id ) if @site
+    respond_with_asset(@picture)
   end
-  
+
   def destroy
     @picture.destroy
     respond_with(@picture, :location => pictures_path)
   end
-  
+
   protected
-  
-    def find_asset
-      @picture = Ckeditor.picture_model.get!(params[:id])
+
+  def find_asset
+    @picture = Ckeditor.picture_model.get!(params[:id])
+  end
+
+  def init_site
+    @account = Account.find_by_host(request.subdomain)
+
+    if params[:current_site].present?
+      @site = @account.sites.find_by_host(params[:current_site])
     end
+  end
+
 end
